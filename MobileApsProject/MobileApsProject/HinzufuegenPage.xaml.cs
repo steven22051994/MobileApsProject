90d6c61ba2e89070f5f00e12e350b8ea36477056
Steven Schirnhofer <ITN249864@qualifizierung.at>
2021-09-21 13:21:12 +0200

    LayoutComplete

---
+++ MobileApsProject/MobileApsProject/HinzufuegenPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApsProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HinzufuegenPage : ContentPage
+    {
+        public HinzufuegenPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
